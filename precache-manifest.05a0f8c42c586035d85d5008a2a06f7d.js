1bd9420e5edc3509d54897cbe33b82ba81e719fa
emil fermanli <emilfermanli@gmail.com>
2020-01-09 14:28:36 +0400

    Updates

---
+++ precache-manifest.05a0f8c42c586035d85d5008a2a06f7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670bc14e84007c8ab95e00184ca41000",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "1a71b67dfa1c932a4dd5",
+    "url": "/pokedex/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "55168f993a28d7f38ff6",
+    "url": "/pokedex/static/css/main.7dc3b86a.chunk.css"
+  },
+  {
+    "revision": "1a71b67dfa1c932a4dd5",
+    "url": "/pokedex/static/js/2.889dd857.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/pokedex/static/js/2.889dd857.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55168f993a28d7f38ff6",
+    "url": "/pokedex/static/js/main.6b35d4c1.chunk.js"
+  },
+  {
+    "revision": "2a765f674afde3da5738",
+    "url": "/pokedex/static/js/runtime-main.1bc26823.js"
+  }
+]);
